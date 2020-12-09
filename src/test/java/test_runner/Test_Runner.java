8fc5a0b6937074f6cca547591b702583ac0b4c18
Queeni-M <queenimuthabathula7@gmail.com>
2020-12-09 10:08:16 +0530

    Project

--- src/test/java/test_runner/Test_Runner.java
+++ src/test/java/test_runner/Test_Runner.java
@@ -1,17 +1,18 @@
 package test_runner;
 
-//import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
-//import cucumber.api.junit.Cucumber;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
+//@RunWith(Cucumber.class) 
+@CucumberOptions(features="Features", glue={"step_def"},
+plugin = {"json:target/cucumber.json"},
+//tags={"@Add_to_cart"}
+tags={"@Successful_login,@Unsuccessful_login"})
+//tags={"@Successful_Registration,@first_name_field_validation,@last_name_field_validation,@email_field_validation,@password_field_validation,@confirm_password_field_validation"})
+public class Test_Runner extends AbstractTestNGCucumberTests{
 
  
 
-//@RunWith(Cucumber.class) 
-@CucumberOptions(features="Features", glue={"step_def"},
-tags={"@Add_to_cart"})
-
-public class Test_Runner extends AbstractTestNGCucumberTests {
+ 
 
  
 
